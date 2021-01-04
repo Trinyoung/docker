ff3c484eca1cd00439e8db6c2ea39923936dd731
trinyoung <trinyoung@163.com>
2021-01-04 20:57:03 +0800

    fix(server): 修改server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-01-04 18:57:06
- * @LastEditTime: 2021-01-04 19:28:43
+ * @LastEditTime: 2021-01-04 20:56:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \docker\server.js
@@ -9,7 +9,7 @@
 const express = require('express');
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const app = express;
+const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
